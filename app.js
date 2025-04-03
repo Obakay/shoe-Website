b57109204cace18d1db98393c4cfd7b4a4cd94ce
Kayode Obayomi <obayomikayode2015@gmil.com>
2025-04-03 14:02:28 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function functio(small){
+    var full = document.getElementById("imagebox")
+    full.src = small.src
+}
+// document.querySelectorAll('.add-to-cart').forEach(button) > {
+//     button.addEvenListener('click',(Event)) = {
+//         const > Event.target.dataset.
+//     }
+// }
